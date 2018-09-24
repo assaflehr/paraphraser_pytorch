d997266f6ab4f1799cae3c4559a43a890d84b86a
assaflehr <assaflehr@gmail.com>
2018-09-24 23:34:01 +0300

    ll

--- paraph/options.py
+++ paraph/options.py
@@ -15,7 +15,7 @@ def get_options(from_sysargv=False):
     parser.add_argument('--beta1', default=0.5, type=float, help='momentum term for adam')
 
     parser.add_argument('--semantics_dim', type=int, default=256, help='size of the semantics vector.default 256')
-    parser.add_argument('--style_dim', type=int, default=1, help='size of the style vector.default 1') # was 60
+    parser.add_argument('--style_dim', type=int, default=100, help='size of the style vector.default 1') # was 60
     parser.add_argument('--sd_weight', type=float, default=1, help='weight on adversarial loss 0.0001 originally. 0.5 is good value!')
     parser.add_argument('--sem_sim_weight', type=float, default=1, help='weight on semantic similiarity loss')
 
